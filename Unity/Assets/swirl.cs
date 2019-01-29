385d5f0bca63b219ea12e8e247afc115af566fcb
EmeraldBamboo <33010394+chen284y@users.noreply.github.com>
2019-01-29 10:51:23 -0600

    update2

---
+++ Unity/Assets/swirl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swirl : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] plates;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < 7; i++)
+        {
+            float j = Random.Range(-0.1f,0.1f);
+            plates[i].gameObject.transform.localScale += new Vector3(j, 0, j);
+        }
+	}
+}
